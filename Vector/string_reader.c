e80a2f478c38f8d26552036addad03a83385e77a
Patrick Jakobsen <lollipop.zandronum@gmail.com>
2017-12-14 17:45:12 +0100

    String Reader comments & cleanup

--- Vector/string_reader.c
+++ Vector/string_reader.c
@@ -1,93 +1,65 @@
+#include <stdio.h>
 #include <string.h>
 
 #include "string_reader.h"
 #include "vector.h"
 
-extern void printVector(Vector *v);
-
+//This function acts as a qsort compliant wrapper for strcmp.
 int lexcmp(const void *s1_, const void *s2_) {
-
+  //Get the data values in the format required by strcmp.
   char *s1 = *(char**)s1_;
   char *s2 = *(char**)s2_;
 
+  //Do the actual comparison.
   int cmp = strcmp(s1,s2);
 
-  //printf("LEXCMP: %s %s %d\n", s1, s2, cmp);
-
+  //Set the return value. The magnitude shouldn't be significant, but this is the format found in various documentation of qsort and comparators.
   if(cmp > 0) {return 1;}
   if(cmp < 0) {return -1;}
   return 0;
 }
 
+//Actual string reader program.
 int main(int argc,char **argv) {
 
+  //Sanity check.
   if(argc < 2) {
     fprintf(stderr, "ERROR: NO INPUT ARGUMENT GIVEN.\n");
     return 1;
   }
 
+  //Open the file and abort if it cannot be opened.
   FILE *f = fopen(argv[1], "r");
   if(f == NULL) {
     fprintf(stderr, "ERROR: UNABLE TO READ FILE.\n");
     return 1;
   }
 
+  //Set up the vector.
   Vector stringVector;
   vector_init(&stringVector);
 
-  char *inStr; // = malloc(sizeof(char) * 1024);
-  //char inStr[1024]; //Buffer
+  //Input string.
+  char *inStr;
 
-  //int code = fscanf(f, "%[^\n]", inStr);
-  //printf("#args successfully filled: %d\n", code);
-
-  //printf("> READING INPUT FILE:\n");
-  /*while(fscanf(f, "%[^\n]\n", inStr) > 0) {
-    //printf("String read: %s\n", inStr);
-    vector_push(&stringVector, inStr);
-    inStr = malloc(sizeof(char) * 1024);
-  }*/
-
+  //Read all lines in the file into the vector.
   while(feof(f) == 0) {
     inStr = (char*) malloc(sizeof(char) * 1024);
     fgets(inStr, sizeof(char) * 1024, f);
     vector_push(&stringVector, inStr);
   }
 
-
-
-  //int code = fscanf(f, "%[^\n]\n", inStr);
-  //printf("#args successfully filled: %d\n", code);
-  //printf("%s\n", inStr);
-
-  //printVector(&stringVector);
-  /*
-  for(int i = 0; i < vector_size(&stringVector); i++) {
-    printf("%s\n", (char*) vector_get_element(&stringVector, i));
-  }*/
-
-  //vector_get_array(&stringVector);
-
-  //printf("     %p");
-
+  //Sort with quicksort, using the comparator above.
   qsort(vector_get_array(&stringVector), vector_size(&stringVector), sizeof(char*), lexcmp);
 
-  //printf("-----\n");
-
+  //Write out all the strings.
   for(size_t i = 0; i < vector_size(&stringVector); i++) {
     printf("%s", (char*) vector_get_element(&stringVector, i));
   }
-  /*
-  char* s1 = "ava";
-  char* s2 = "vsd";
 
-  printf("%d %d\n", strcmp(s1, s2), lexcmp((void*)s1, (void*)s2));
-
-  */
-
+  //Neatly close the file.
   fclose(f);
 
-
-
+  //We're done. Signal successful execution.
   return 0;
 }
--- Vector/string_reader.h
+++ Vector/string_reader.h
@@ -1,16 +1,13 @@
 #ifndef STRING_READER_H
 #define STRing_READER_H
 
-#include <stdio.h>
-
 /*
  * String Reader.
  * This program reads newline-separated string from a file given as command line argument.
  *  The strings read are written out to stdout in lexicographically sorted order.
 */
 
-
-
-
+//This function acts as a qsort compliant wrapper for strcmp.
+int lexcmp(const void *s1_, const void *s2_);
 
 #endif
